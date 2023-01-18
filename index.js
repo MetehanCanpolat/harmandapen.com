b5f05b0295629658b53bc67502afcf0c69a95cbe
MetehanCanpolat <120008642+MetehanCanpolat@users.noreply.github.com>
2023-01-18 15:48:16 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
   }
+  
+  function myMap() {
+    var mapProp = {
+      center: new google.maps.LatLng(51.508742, -0.12085),
+      zoom: 5,
+    };
+    var map = new google.maps.Map(
+      document.getElementById("googleMap"),
+      mapProp
+    );
+  }
